6ab11f1010b39b43def21d8942def8ae5ec92993
unique-username313 <unique_username313@protonmail.com>
2020-07-18 15:17:44 +0300

    add alacritty-rc cask

---
+++ Casks/alacritty-rc.rb
@@ -0,0 +1,16 @@
+cask 'alacritty' do
+  version '0.5.0-rc1'
+  sha256 :no_check
+
+  url "https://github.com/alacritty/alacritty/releases/download/v#{version}/Alacritty-v#{version}.dmg"
+  appcast 'https://github.com/alacritty/alacritty/releases.atom'
+  name 'Alacritty'
+  homepage 'https://github.com/alacritty/alacritty/'
+
+  app 'Alacritty.app'
+  binary "#{appdir}/Alacritty.app/Contents/MacOS/alacritty"
+
+  zap delete: [
+                '~/Library/Saved Application State/io.alacritty.savedState',
+              ]
+end
